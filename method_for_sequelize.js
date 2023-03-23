ee40d950017287ebebfe8ad64efb47dbcbcdc981
laurent-bras <98352346+laurent-bras@users.noreply.github.com>
2023-03-23 10:06:52 +0100

    Update method_for_sequelize.js

--- method_for_sequelize.js
+++ method_for_sequelize.js
@@ -1,46 +1,61 @@
 const { Op } = require('sequelize') 
-const { Car } = require('../models');
+const { Car, Wheel } = require('../models');
 const { setReadOnlyOnCars } = require('../tools/read-only');
-const { getListOfFriends } = require('../tools/rules');
+const { getListOfGarages } = require('../tools/rules');
 
-const { CarDelete } = require('./eltlish');
-const { manageExportByCar } = require('./exporteltlish');
-
-
-async function asyncGetCarsByOwnerId(ownerInputId) {
-    let friends = await getListOfFriends(ownerInputId);
-    if (!friends) return null;
-    let ownerList2 = friends.map((own) => own.uid);
-    /* xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx */
-    let myCarList = await Car.findAll({
-        where: {
-            ownerId: {
-                [Op.in]: ownerList2,   
-            }
-        },
-        order: [[eltlish,'priority','ASC']],
-        include: 
-            [{
-                model: eltlish,
-                include: [
-                    { model: XXXX },
-                    { model: YYYY }
-                ],
-            }]
-    });
-
-    let myCarList2 = myCarList.map((Car) => Car.toJSON());
-
-    // check read only 
-    let myCarList3 = await setReadOnlyOnCars(myCarList2, ownerInputId)
+const getCarsByOwnerId = (ownerId) => {
+    return new Promise( async (resolve, reject) => {
+        
+        let garages = [];
+        try {
+            garages = await getListOfGarages(ownerId);
+            
+        } catch (error) {
+            return reject({
+                msg: "Failed to retrieve list of garages"
+            })
+        }
+        if (garages.length < 1) {
+            return resolve({
+                msg: "List of garages empty",
+                result: [],
+            })
+        };
+        const owners = garages.map((own) => own.uid);
     
-    // set read only in eltlishes as Cars
-    myCarList3.forEach(Car => {
-        let readOnlyCar = Car.readOnly;
-        Car.eltlishes.forEach(elt => {
-            elt.readOnly = readOnlyCar
+        const cars = await Car.findAll({
+            where: {
+                ownerId: {
+                    [Op.in]: owners,   
+                }
+            },
+            order: [['color','ASC']],
+            include: 
+                [{
+                    model: Wheel,
+                }]
+        });
+    
+        const cars2 = cars.map((car) => car.toJSON());
+    
+        // check read only 
+        const cars3 = await setReadOnlyOnCars(cars2, ownerId);
+        
+        // set read only on wheels like cars
+        for (const car3 of cars3) {
+            const readOnlyCar = car3.readOnly;
+            for (const wheel of car3.wheels) {
+                wheel.readOnly = readOnlyCar;
+            }
+        }
+    
+        resolve({
+            msg: "ok",
+            result: cars3,
         })
     });
-
-    return myCarList3;
 }
+
+module.exports = {
+    getCarsByOwnerId
+};
